2506622a062c70c41061bb546c96952fe511fc90
alicemw <1660523599@qq.com>
2018-02-05 16:42:03 +0800

    js06,正则表达式

---
+++ bingbing/js05/index.js
@@ -0,0 +1,20 @@
+/*冒泡
+ *排序
+ */
+
+var arr = [1,2,4,6,7,3,5,9,12,44];
+/*for(var i=0;i<arr.length;++i){
+	var d;
+	for(var j =0;j<arr.length;++j){
+		if(arr[i]>arr[j]){
+			d = arr[i];
+			arr[i] =arr[j];
+			arr[j] = d;
+		}
+	}
+}*/
+/*
+ 快速排序
+ * */
+
+console.log(arr)
